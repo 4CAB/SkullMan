f6656f2e365bde83aabd66822dc2bf839078bf52
4CAB <72951815+4CAB@users.noreply.github.com>
2021-01-04 18:23:08 +0100

    Add files via upload

---
+++ KeepAlive.py
@@ -0,0 +1,14 @@
+import os
+
+num = 0
+if == 0:
+	os.system("pip3 install python3-xlib")
+	os.system("sudo apt-get install scrot")
+	os.system("sudo apt-get install python3-tk")
+	os.system("sudo apt-get install python3-dev")
+	os.system("pip3 install pyautogui")
+	num += 1
+
+while True:
+	os.system("python3 BotCrack.py")
+	print("Reconectando . . .")
