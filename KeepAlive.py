d81e43cc706c066b42f4d850d14fb504e7fa720a
4CAB <72951815+4CAB@users.noreply.github.com>
2021-01-04 18:28:50 +0100

    Update KeepAlive.py

--- KeepAlive.py
+++ KeepAlive.py
@@ -1,12 +1,18 @@
 import os
+import time 
 
 num = 0
 if num == 0:
 	os.system("pip3 install python3-xlib")
+	time.sleep(10)
 	os.system("sudo apt-get install scrot")
+	time.sleep(10)
 	os.system("sudo apt-get install python3-tk")
+	time.sleep(10)
 	os.system("sudo apt-get install python3-dev")
+	time.sleep(10)
 	os.system("pip3 install pyautogui")
+	time.sleep(10)
 	num += 1
 
 while True:
